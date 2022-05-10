b3983c4bda0b5cb55b517d9d3fd162548be17e93
IsaacKerson <102610344+IsaacKerson@users.noreply.github.com>
2022-05-11 01:53:53 +0900

    added title

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ import sqlite3
 
 conn = sqlite3.connect('vocabulary.db')
 c = conn.cursor()
-unit = ("Unit 1",)
+unit = "Unit 1"
+unit_tup = (unit,)
 
-st.title("With SQLite")
-for row in c.execute("SELECT * FROM vocab WHERE unit = ?", unit):
+st.title(unit)
+for row in c.execute("SELECT * FROM vocab WHERE unit = ?", unit_tup):
     st.write(row[2])
 
 conn.close()
