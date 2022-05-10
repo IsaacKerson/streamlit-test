2436f8bb3975580429bcf1205af20aa05bf6d856
IsaacKerson <102610344+IsaacKerson@users.noreply.github.com>
2022-05-11 00:01:07 +0900

    add server_api and change pw

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 import streamlit as st
 import pymongo
 
+server_api = pymongo.server_api.ServerApi('1')
+uri = "mongodb+srv://m001-student:test@sandbox.zqzg8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
-uri = "mongodb+srv://m001-student:test-run@sandbox.zqzg8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-client = pymongo.MongoClient(uri)
+client = pymongo.MongoClient(uri, server_api=server_api)
 mflix = client.sample_mflix
 movies = mflix.movies
 
 st.title("With Pymongo")
-
-#x = st.slider('Select a value')
-#st.write(x, 'squared is', x * x)
 st.write(movies.find_one())
