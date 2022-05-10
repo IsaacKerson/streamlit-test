5a87f312f7e6d593b44d9b31ae7e11bf7bae1d25
IsaacKerson <102610344+IsaacKerson@users.noreply.github.com>
2022-05-11 01:12:08 +0900

    SQLite Test

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 import streamlit as st
-import pymongo
+import sqlite3
 
-uri = "mongodb+srv://m220student:test@sandbox.zqzg8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-client = pymongo.MongoClient(uri, connect=False)
-mflix = client.sample_mflix
-
-movies = mflix.movies
-
-st.title("With Pymongo")
-st.write(movies.find_one())
+conn = sqlite3.connect('vocabulary.db')
+cursor = conn.cursor
+unit = ("Unit 1",)
+cursor.execute("SELECT * FROM vocab WHERE unit = ?", unit)
+st.title("With SQLite")
+st.write(cursor.fetchone())
