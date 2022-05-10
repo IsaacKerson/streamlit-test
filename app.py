6892bccc6ec95965e867d81059444acdc0a53e54
IsaacKerson <102610344+IsaacKerson@users.noreply.github.com>
2022-05-11 00:08:28 +0900

    new password attempt

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 import streamlit as st
 import pymongo
 
-server_api = pymongo.server_api.ServerApi('1')
-uri = "mongodb+srv://m001-student:test@sandbox.zqzg8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-
-client = pymongo.MongoClient(uri, server_api=server_api)
+uri = "mongodb+srv://m220student:test@sandbox.zqzg8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+client = pymongo.MongoClient(uri)
 mflix = client.sample_mflix
+
 movies = mflix.movies
 
 st.title("With Pymongo")
