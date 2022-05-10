0e7095e7e78b73564ef9aaee87444630acf9d37f
IsaacKerson <102610344+IsaacKerson@users.noreply.github.com>
2022-05-11 01:18:03 +0900

    updated cursor object

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 import sqlite3
 
 conn = sqlite3.connect('vocabulary.db')
-cursor = conn.cursor
+c = conn.cursor()
 unit = ("Unit 1",)
-cursor.execute("SELECT * FROM vocab WHERE unit = ?", unit)
+c.execute("SELECT * FROM vocab WHERE unit = ?", unit)
 st.title("With SQLite")
-st.write(cursor.fetchone())
+st.write(c.fetchone()))
