022197bcdebcff0cd3f6983c795c1abe395bbc3e
IsaacKerson <102610344+IsaacKerson@users.noreply.github.com>
2022-05-11 00:21:39 +0900

    updated uri

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pymongo
 
-uri = "mongodb+srv://m220student:test@sandbox.zqzg8.mongodb.net/sample_mflix/movies?ssl=true&ssl_cert_reqs=CERT_NONE"
+uri = "mongodb+srv://m220student:test@sandbox.zqzg8.mongodb.net/sample_mflix?ssl=true&ssl_cert_reqs=CERT_NONE"
 client = pymongo.MongoClient(uri)
 mflix = client.sample_mflix
 
