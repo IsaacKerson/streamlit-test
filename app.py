e153455c67093537c9c0ab45d01ff78984ec5f15
IsaacKerson <102610344+IsaacKerson@users.noreply.github.com>
2022-05-10 23:13:15 +0900

    set up mongo connection

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 import streamlit as st
-st.title("This is an update to the app.")
-x = st.slider('Select a value')
-st.write(x, 'squared is', x * x)
+import pymongo
+
+
+cluster = "mongodb+srv://m001-student:test-run@sandbox.zqzg8.mongodb.net/sample_airbnb?retryWrites=true&w=majority"
+client = pymongo.MongoClient(cluster, server_api=ServerApi('1'))
+#db = client.test
+
+st.title("With Pymongo")
+
+#x = st.slider('Select a value')
+#st.write(x, 'squared is', x * x)
+st.write(client.list_database_names())
