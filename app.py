f43f9dce4cb131ae3a374ff4478180bae9753b8e
IsaacKerson <102610344+IsaacKerson@users.noreply.github.com>
2022-05-10 23:41:47 +0900

    add pymongo[srv]

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ import streamlit as st
 import pymongo
 
 
-cluster = "mongodb+srv://m001-student:test-run@sandbox.zqzg8.mongodb.net/sample_airbnb?retryWrites=true&w=majority"
-server_api = pymongo.server_api.ServerApi('1')
-client = pymongo.MongoClient(cluster, server_api=server_api)
-#db = client.test
+uri = "mongodb+srv://m001-student:test-run@sandbox.zqzg8.mongodb.net"
+client = pymongo.MongoClient(uri)
+mflix = client.sample_mflix
+movies = mflix.movies
 
 st.title("With Pymongo")
 
 #x = st.slider('Select a value')
 #st.write(x, 'squared is', x * x)
-st.write(client.list_database_names())
+st.write(movies.find_one())
