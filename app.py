4ba37ec12accea5ee577bb9bbb855db0a9824409
IsaacKerson <102610344+IsaacKerson@users.noreply.github.com>
2022-05-11 13:47:49 +0900

    moved title to top

--- app.py
+++ app.py
@@ -14,13 +14,13 @@ units_list = []
 for item in c.execute("SELECT DISTINCT unit FROM vocab"):
   units_list.append(item[0])
 
+st.title("Sentence Completion")
 st.selectbox('Select a unit.', units_list, key='unit')
 st.selectbox('How many question do you want?', [5,10,15,20], key='num_q')
+
 unit = st.session_state.unit
 num_q = st.session_state.num_q
 input_tup = (unit, num_q)
-
-st.title("Sentence Completion")
 
 st.header(unit)
 
