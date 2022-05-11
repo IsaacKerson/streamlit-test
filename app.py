e18dd97bd1c308d8629d017831b6033c687a4c61
IsaacKerson <102610344+IsaacKerson@users.noreply.github.com>
2022-05-11 11:37:45 +0900

    get unit list

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 import streamlit as st
 import sqlite3
 
+
+
 conn = sqlite3.connect('vocabulary.db')
 c = conn.cursor()
 unit = "Unit 1"
 unit_tup = (unit,)
 
 st.title(unit)
-for row in c.execute("SELECT * FROM vocab WHERE unit = ?", unit_tup):
-    st.write(row[2])
+
+units_list = []
+for item in c.execute("SELECT DISTINCT unit FROM vocab"):
+  units_list.append(item[0])
+st.write(units_list)
+
+# for row in c.execute("SELECT * FROM vocab WHERE unit = ?", unit_tup):
+#     st.write(row[2])
 
 conn.close()
