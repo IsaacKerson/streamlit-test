385b5abc9f5e60503bb240ecc89c7d73fb367934
IsaacKerson <102610344+IsaacKerson@users.noreply.github.com>
2022-05-11 11:48:42 +0900

    add session state for unit

--- app.py
+++ app.py
@@ -5,15 +5,16 @@ import sqlite3
 
 conn = sqlite3.connect('vocabulary.db')
 c = conn.cursor()
-unit = "Unit 1"
-unit_tup = (unit,)
-
-st.title(unit)
 
 units_list = []
 for item in c.execute("SELECT DISTINCT unit FROM vocab"):
   units_list.append(item[0])
-st.selectbox('Select a unit.', units_list)
+
+st.selectbox('Select a unit.', units_list, key='unit')
+unit = st.session_state.unit
+unit_tup = (unit,)
+
+st.title(unit)
 
 # for row in c.execute("SELECT * FROM vocab WHERE unit = ?", unit_tup):
 #     st.write(row[2])
