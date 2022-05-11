2a5ad6d8bc70d39e2c2f5dcb355412ca3f52b8f9
IsaacKerson <102610344+IsaacKerson@users.noreply.github.com>
2022-05-11 12:12:25 +0900

    add question number choice

--- app.py
+++ app.py
@@ -11,12 +11,14 @@ for item in c.execute("SELECT DISTINCT unit FROM vocab"):
   units_list.append(item[0])
 
 st.selectbox('Select a unit.', units_list, key='unit')
+st.selectbox('How many question do you want?', [5,10,15,20], key='num_q')
 unit = st.session_state.unit
-unit_tup = (unit,)
+num_q = st.session_state.num_q
+input_tup = (unit, num_q)
 
 st.title(unit)
 
-for row in c.execute("SELECT * FROM vocab WHERE unit = ? LIMIT 10", unit_tup):
+for row in c.execute("SELECT * FROM vocab WHERE unit = ? LIMIT ?", input_tup):
     st.write(row[2])
 
 conn.close()
