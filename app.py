ca598164c14322d8071c0e1a91eca47625cc93aa
IsaacKerson <102610344+IsaacKerson@users.noreply.github.com>
2022-05-11 13:42:00 +0900

    add word bank

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import sqlite3
 def add_blanks(word, sentence, blank = "__"):
   return sentence.replace(word, blank)
 
+def chunker(seq, size):
+    return (seq[pos:pos + size] for pos in range(0, len(seq), size))
+
 conn = sqlite3.connect('vocabulary.db')
 c = conn.cursor()
 
@@ -20,13 +23,18 @@ input_tup = (unit, num_q)
 st.title(unit)
 
 questions = []
+word_bank = []
 
 query = "SELECT * FROM vocab WHERE unit = ? ORDER BY RANDOM() LIMIT ?"
 
 for idx, item in enumerate(c.execute(query, input_tup)):
     word = item[2]
+    word_bank.append(word)
     sentence = item[4]
     questions.append((idx + 1, word, sentence, add_blanks(word, sentence)))
+
+st.subheader("Word Bank")
+st.table(chunker(word_bank, 5))
 
 for q in questions:
     st.write(f'{q[0]}. {q[3]}')
