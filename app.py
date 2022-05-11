eb637f65d2ef2af459e749a509d26a9e394f35f4
IsaacKerson <102610344+IsaacKerson@users.noreply.github.com>
2022-05-11 11:42:38 +0900

    add select  box

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ st.title(unit)
 units_list = []
 for item in c.execute("SELECT DISTINCT unit FROM vocab"):
   units_list.append(item[0])
-st.write(units_list)
+st.selectbox(units_list)
 
 # for row in c.execute("SELECT * FROM vocab WHERE unit = ?", unit_tup):
 #     st.write(row[2])
