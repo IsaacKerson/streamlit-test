13955c286bfb6b48c5d095a49ff7fcbf90813cb9
IsaacKerson <102610344+IsaacKerson@users.noreply.github.com>
2022-05-11 16:11:36 +0900

    add form submssion check

--- app.py
+++ app.py
@@ -8,55 +8,50 @@ def add_blanks(word, sentence, blank = "__"):
 def chunker(seq, size):
     return (seq[pos:pos + size] for pos in range(0, len(seq), size))
 
-def form_callback():
-    st.write(st.session_state.q_1)
-    st.write(st.session_state.q_2)
+if st.session_state.form_submit == True:
+    for k, v in st.session_state.items():
+        st.write(k, v)
+else: 
+    conn = sqlite3.connect('vocabulary.db')
+    c = conn.cursor()
 
-conn = sqlite3.connect('vocabulary.db')
-c = conn.cursor()
+    units_list = []
+    for item in c.execute("SELECT DISTINCT unit FROM vocab"):
+    units_list.append(item[0])
 
-units_list = []
-for item in c.execute("SELECT DISTINCT unit FROM vocab"):
-  units_list.append(item[0])
+    st.title("Sentence Completion")
+    st.selectbox('Select a unit.', units_list, key='unit')
+    st.selectbox('How many question do you want?', [5,10,15,20], key='num_q')
 
-st.title("Sentence Completion")
-st.selectbox('Select a unit.', units_list, key='unit')
-st.selectbox('How many question do you want?', [5,10,15,20], key='num_q')
+    unit = st.session_state.unit
+    num_q = st.session_state.num_q
+    input_tup = (unit, num_q)
 
-unit = st.session_state.unit
-num_q = st.session_state.num_q
-input_tup = (unit, num_q)
+    st.header(unit)
 
-st.header(unit)
+    st.write("Complete the sentences with the words from the word bank.")
 
-st.write("Complete the sentences with the words from the word bank.")
+    questions = []
+    word_bank = []
 
-questions = []
-word_bank = []
+    query = "SELECT * FROM vocab WHERE unit = ? ORDER BY RANDOM() LIMIT ?"
 
-query = "SELECT * FROM vocab WHERE unit = ? ORDER BY RANDOM() LIMIT ?"
+    for idx, item in enumerate(c.execute(query, input_tup)):
+        word = item[2]
+        word_bank.append(word)
+        sentence = item[4]
+        questions.append((idx + 1, word, sentence, add_blanks(word, sentence)))
 
-for idx, item in enumerate(c.execute(query, input_tup)):
-    word = item[2]
-    word_bank.append(word)
-    sentence = item[4]
-    questions.append((idx + 1, word, sentence, add_blanks(word, sentence)))
+    st.subheader("Word Bank")
+    random.shuffle(word_bank)
+    st.table(chunker(word_bank, 5))
 
-st.subheader("Word Bank")
-random.shuffle(word_bank)
-st.table(chunker(word_bank, 5))
-
-if 'form_submit' not in st.session_state:
     with st.form("sentence_completion"):
         for q in questions:
             st.text_input(f'{q[0]}. {q[3]}', key=f'q_{q[0]}', placeholder="Type answer here")
-        submitted = st.form_submit_button(label="Submit", on_click=form_callback)
+        submitted = st.form_submit_button(label="Submit")
         if submitted:
             st.session_state.questions = questions
             st.session_state.form_submit = True
             st.write(st.session_state.form_submit)
-
-for k, v in st.session_state.items():
-    st.write(k, v)
-    
-conn.close()
+    conn.close()
