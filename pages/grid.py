78ab07d74b4b975b21e8702eff5ef123739910f7
IsaacKerson <102610344+IsaacKerson@users.noreply.github.com>
2022-05-13 19:17:33 +0900

    add grid with forms

--- pages/grid.py
+++ pages/grid.py
@@ -1,31 +1,18 @@
 import streamlit as st
-import pandas as pd
 import numpy as np
+import pandas as pd
 
-from st_aggrid import AgGrid, GridOptionsBuilder, JsCode
+from st_aggrid import AgGrid, DataReturnMode, GridUpdateMode, GridOptionsBuilder
 
-def app():
-    df = pd.DataFrame(
-        "",
-        index=range(10),
-        columns=list("abcde"),
-    )
+df_template = pd.DataFrame(
+    '',
+    index=range(10),
+    columns=list('abcde')
+)
 
-    gb = GridOptionsBuilder.from_dataframe(df)
-    gb.configure_default_column(editable=True)
+with st.form('example form') as f:
+    st.header('Example Form')
+    response = AgGrid(df_template, editable=True, fit_columns_on_grid_load=True)
+    st.form_submit_button()
 
-    gb.configure_column('a',
-        cellEditor='agRichSelectCellEditor',
-        cellEditorParams={'values':['a','b','c']}
-    )
-
-    gb.configure_grid_options(enableRangeSelection=True)
-
-
-    response = AgGrid(
-        df,
-        gridOptions=gb.build(),
-        fit_columns_on_grid_load=True,
-        allow_unsafe_jscode=True,
-        enable_enterprise_modules=True
-    )
+st.write(response['data'])  
