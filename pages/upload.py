1436dfd262989e3a0627c705dab9f52976844425
IsaacKerson <102610344+IsaacKerson@users.noreply.github.com>
2022-05-14 22:15:45 +0900

    write out csv files

--- pages/upload.py
+++ pages/upload.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-from io import StringIO
+import csv
 
 from pages.utils import *
 
@@ -16,13 +16,18 @@ def app():
     # Code to read a single file 
     uploaded_file = st.file_uploader("Choose a file", type = ['csv', 'xlsx'])
     if uploaded_file is not None:
-        stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-        st.write(stringio)
-    #     try:
-    #         data = pd.read_csv(uploaded_file)
-    #     except Exception as e:
-    #         print(e)
-    #         data = pd.read_excel(uploaded_file)
+        try:
+            data = pd.read_csv(uploaded_file)
+            data.to_csv('data.csv', index=False)
+        except Exception as e:
+            print(e)
+            data = pd.read_excel(uploaded_file)
+            data.to_csv('data.csv', index=False)
+
+        with open("data.csv", "r") as f:
+            reader = csv.reader(f, delimiter=",")
+            for i, line in enumerate(reader):
+                st.write(f"{i}: {line}")
     
     # if st.button("Load Data"):
     #     st.markdown("### Uploaded Data")
