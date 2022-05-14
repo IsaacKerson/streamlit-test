4e488180827c0fc5906477154568d08a3b3d0550
IsaacKerson <102610344+IsaacKerson@users.noreply.github.com>
2022-05-14 16:29:36 +0900

    remove grid import from app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import datetime
 
 # Custom imports
 from multipage import MultiPage
-from pages import quiz, upload, view, grid
+from pages import quiz, upload, view
 
 # Create an instance of the app 
 app = MultiPage()
