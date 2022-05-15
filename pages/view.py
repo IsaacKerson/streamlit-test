035a7298b1e00671b226678dae0d2b92907c7dc2
IsaacKerson <102610344+IsaacKerson@users.noreply.github.com>
2022-05-15 20:06:57 +0900

    round kilobytes

--- pages/view.py
+++ pages/view.py
@@ -24,7 +24,7 @@ def app():
     
     c.execute(size_query)
     
-    st.markdown(f'##### Database size: {c.fetchone()[0] / 1000} kilobytes')
+    st.markdown(f'##### Database size: {round(c.fetchone()[0] / 1000, 0} KB')
 
     query = st.text_input("Query", placeholder="Type query here")
 
