e6a319a11ba405b55d20af72a6f842b96f01c40a
IsaacKerson <102610344+IsaacKerson@users.noreply.github.com>
2022-05-15 12:35:32 +0900

    add callback to upload page

--- pages/upload.py
+++ pages/upload.py
@@ -10,34 +10,39 @@ def app():
     '''delete form_submit to run quiz maker on return to page'''
     if "form_submit" in st.session_state.keys():
         del st.session_state.form_submit
+    
+    def upload_callback(data):
+        st.session_state.form_upload = True
+        st.write(data)
 
-    st.markdown("## Upload Data")
+    if "form_upload" not in st.session_state:
+        st.markdown("## Upload Data")
 
-    # Code to read a single file 
-    uploaded_file = st.file_uploader("Choose a file", type = ['csv', 'xlsx'])
-    if uploaded_file is not None:
-        try:
-            data = pd.read_csv(uploaded_file)
-            data.to_csv('data.csv', index=False)
-        except Exception as e:
-            print(e)
-            data = pd.read_excel(uploaded_file)
-            data.to_csv('data.csv', index=False)
-  
-    if st.button("Load Data"):
-        st.markdown("### Data")
-        form = st.form("data_check_form")
-        with open("data.csv", "r") as f:
-            reader = csv.reader(f, delimiter=",")
-            for i, line in enumerate(reader):
-                if i == 0:
-                    pass
-                else:
-                    form.markdown(f"### {i}")
-                    form.text_input("Word or Phrase", f"{line[0]}", key=f"word_{i}")
-                    form.text_input("Definition", f"{line[1]}", key=f"def_{i}")
-                    form.text_input("Example", f"{line[2]}", key=f"ex_{i}")
-                    form.text_input("Tags", f"{line[3]}", key=f"tag_{i}")
-        form.form_submit_button("Submit")
-# st.text_input(f'{q[0] + 1}. {q[3]}', key=q[0], placeholder="Type answer here")
-# st.form_submit_button(label="Submit", on_click=form_callback, args=(questions,))
+        # Code to read a single file 
+        uploaded_file = st.file_uploader("Choose a file", type = ['csv', 'xlsx'])
+        if uploaded_file is not None:
+            try:
+                data = pd.read_csv(uploaded_file)
+                data.to_csv('data.csv', index=False)
+            except Exception as e:
+                print(e)
+                data = pd.read_excel(uploaded_file)
+                data.to_csv('data.csv', index=False)
+    
+        if st.button("Load Data"):
+            st.markdown("### Confirm the data is correct.")
+            form = st.form("data_check_form")
+            with open("data.csv", "r") as f:
+                reader = csv.reader(f, delimiter=",")
+                for i, line in enumerate(reader):
+                    if i == 0:
+                        pass
+                    else:
+                        form.markdown(f"### {i}")
+                        form.text_input("Word or Phrase", f"{line[0]}", key=f"word_{i}")
+                        form.text_input("Definition", f"{line[1]}", key=f"def_{i}")
+                        form.text_input("Example", f"{line[2]}", key=f"ex_{i}")
+                        form.text_input("Tags", f"{line[3]}", key=f"tag_{i}")
+            form.form_submit_button("Confirm", on_click=upload_callback, args=(data,))
+    # st.text_input(f'{q[0] + 1}. {q[3]}', key=q[0], placeholder="Type answer here")
+    # st.form_submit_button(label="Submit", on_click=form_callback, args=(questions,))
