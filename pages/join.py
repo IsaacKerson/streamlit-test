24713d177d2a33ebd7ce449ad4bf87be7853d57b
IsaacKerson <102610344+IsaacKerson@users.noreply.github.com>
2022-05-15 23:25:13 +0900

    fix form error

--- pages/join.py
+++ pages/join.py
@@ -9,11 +9,11 @@ from pages.utils import *
 st.markdown("## Join")
 
 with st.form("join_form"):
-    first_name = form.text_input("First Name")
-    last_name = form.text_input("Last Name")
-    user_name = form.text_input("User Name")
-    password1 = form.text_intput("Password", type="password")
-    password2 = form.text_input("Confirm Password", type="password")
-    submitted = form.form_submit_button("Submit")
+    first_name = st.text_input("First Name")
+    last_name = st.text_input("Last Name")
+    user_name = st.text_input("User Name")
+    password1 = st.text_intput("Password", type="password")
+    password2 = st.text_input("Confirm Password", type="password")
+    submitted = st.form_submit_button("Submit")
 if password1.strip() != password2.strip():
     st.warning("The passwords do not match.")
