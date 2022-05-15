2b772aeb01e576049c06cba33f0e6df0b020702c
IsaacKerson <102610344+IsaacKerson@users.noreply.github.com>
2022-05-15 20:01:39 +0900

    fix range error

--- pages/view.py
+++ pages/view.py
@@ -24,7 +24,7 @@ def app():
     
     c.execute(size_query)
     
-    st.markdown(f'#### Database size: {c.fetchone()[1]}')
+    st.markdown(f'#### Database size: {c.fetchone()[0]}')
 
     query = st.text_input("Query", placeholder="Type query here")
 
