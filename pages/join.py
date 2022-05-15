e817f7f1dd704c30edd9b4a2eae2009c93a9e474
IsaacKerson <102610344+IsaacKerson@users.noreply.github.com>
2022-05-15 23:59:34 +0900

    add user and email db check

--- pages/join.py
+++ pages/join.py
@@ -9,6 +9,17 @@ from pages.utils import *
 def app():
 
     DATABASE = db_path('quiz_maker.db')
+    c, conn = db_connect(DATABASE)
+    query = "CREATE TABLE IF NOT EXISTS users(uct_iso, firstname, lastname, username, email, hashed_password)"
+    c.execute(query)
+
+    usernames = []
+    emails = []
+    
+    query = "SELECT username, email FROM users"
+    for items in c.execute(query):
+        usernames.append(items[0])
+        emails.append(items[1])
 
     st.markdown("## Join")
 
@@ -16,6 +27,7 @@ def app():
         first_name = st.text_input("First Name")
         last_name = st.text_input("Last Name")
         user_name = st.text_input("User Name")
+        email = st.text_input("Email")
         password1 = st.text_input("Password", type="password")
         password2 = st.text_input("Confirm Password", type="password")
         
@@ -23,3 +35,7 @@ def app():
 
     if submitted and password1.strip() != password2.strip():
         st.warning("The passwords do not match.")
+    if user_name in usernames:
+        st.warning("This user name already exists.")
+    if email in emails:
+        st.warning("This email is already being used.")
