7b3e91c3da808176e7a069b1a2e9d86baa90e457
IsaacKerson <102610344+IsaacKerson@users.noreply.github.com>
2022-05-15 23:31:38 +0900

    add app fuction to join.py

--- pages/join.py
+++ pages/join.py
@@ -6,16 +6,17 @@ import datetime
 # Custom imports
 from pages.utils import *
 
-st.markdown("## Join")
+def app():
+    st.markdown("## Join")
 
-with st.form("join_form"):
-    first_name = st.text_input("First Name")
-    last_name = st.text_input("Last Name")
-    user_name = st.text_input("User Name")
-    password1 = st.text_input("Password", type="password")
-    password2 = st.text_input("Confirm Password", type="password")
-    
-    submitted = st.form_submit_button("Submit")
+    with st.form("join_form"):
+        first_name = st.text_input("First Name")
+        last_name = st.text_input("Last Name")
+        user_name = st.text_input("User Name")
+        password1 = st.text_input("Password", type="password")
+        password2 = st.text_input("Confirm Password", type="password")
+        
+        submitted = st.form_submit_button("Submit")
 
-if submitted and password1.strip() != password2.strip():
-    st.warning("The passwords do not match.")
+    if submitted and password1.strip() != password2.strip():
+        st.warning("The passwords do not match.")
